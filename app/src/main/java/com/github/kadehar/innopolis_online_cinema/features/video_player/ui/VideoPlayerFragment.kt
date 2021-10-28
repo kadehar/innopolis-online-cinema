84dc6cb9d03a91cda66bbd29840916f001b94609
Кузнецов Алексей Эдуардович <akuznetsov@mpl.lc>
2021-10-28 22:45:36 +0300

    Add video player skeleton

---
+++ app/src/main/java/com/github/kadehar/innopolis_online_cinema/features/video_player/ui/VideoPlayerFragment.kt
@@ -0,0 +1,7 @@
+package com.github.kadehar.innopolis_online_cinema.features.video_player.ui
+
+import androidx.fragment.app.Fragment
+import com.github.kadehar.innopolis_online_cinema.R
+
+class VideoPlayerFragment : Fragment(R.layout.fragment_video_player) {
+}
